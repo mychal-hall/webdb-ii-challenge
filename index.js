88ae1c51fb5619a9464812a85fe254d42b9a7a27
Mychal Hall <hallmychal@gmail.com>
2019-05-25 13:49:43 -0500

    Code clean up

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const server = require("./server.js");
 
-
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
-
-
-
-
-
-
-
